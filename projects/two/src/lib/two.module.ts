5dad246eb884ec41c725e9239436230073a33be2
Alastair Todd <alastair.todd@gmail.com>
2019-04-15 13:42:27 +0100

    update to use library distribution

--- projects/two/src/lib/two.module.ts
+++ projects/two/src/lib/two.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { TwoComponent } from "./two.component";
-import { OneModule } from "projects/one/src/lib/one.module";
+import { OneModule } from "one";
 
 @NgModule({
   declarations: [TwoComponent],
